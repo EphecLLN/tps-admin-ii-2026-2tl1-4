67db21c798cd3d96f2753faf9018a796448b74f7
LeRoiJeanKins <mustafahs1659659@gmail.com>
2026-04-23 08:32:20 +0000

    fin sécurisation server sauf .4

--- server/web/html/www/products.php
+++ server/web/html/www/products.php
@@ -17,10 +17,11 @@
 <h1>Catalogue WoodyToys</h1>
 
 <?php
-$dbname = 'woodytoys';
-$dbuser = 'root';
-$dbpass = 'mypass';
-$dbhost = 'db';
+
+$dbhost = getenv('DB_HOST');
+$dbuser = getenv('DB_USER');
+$dbpass = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
 mysqli_select_db($connect,$dbname) or die("Could not open the database '$dbname'");
 $result = mysqli_query($connect,"SELECT id, product_name, product_price FROM products");
